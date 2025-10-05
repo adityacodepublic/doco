aa989c2893d7d6207f1570ce82305cbd87ed456c
gitcode <website0wordpress@gmail.com>
2025-10-05 17:20:47 +0530

    replace key

--- lib/services/fileProcessor.ts
+++ lib/services/fileProcessor.ts
@@ -22,7 +22,7 @@ const documentSchema = z.object({
 
 export type ProcessedData = z.infer<typeof documentSchema>;
 
-const key = "AIzaSyCZ4WIMyhDULsXkrlgiJxAuUuFnx9gzLO8";
+const key = z.string().min(1).parse(process.env.NEXT_PUBLIC_GOOGLE_API_KEY);
 
 const ai = new GoogleGenAI({ apiKey: key });
 
